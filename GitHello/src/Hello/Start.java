bedebae1fe708039ebd6f56514fcee7dc5b06feb
yongtae <kyt10192006@gmail.com>
2023-02-03 16:48:05 +0900

    My commit

---
+++ GitHello/src/Hello/Start.java
@@ -0,0 +1,14 @@
+package Hello;
+
+public class Start {
+
+	public Start() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
